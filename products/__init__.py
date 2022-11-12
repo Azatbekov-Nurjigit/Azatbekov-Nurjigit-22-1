4f133a6a74cc916d4370b78ecd4753595a25ec88
Azatbekov-Nurjigit <nurrjigit555@gmail.com>
2022-11-12 15:43:06 +0600

    hbfhvbsd

---
+++ products/__init__.py
@@ -0,0 +1,23 @@
+
+
+# Сделать 3 view (function based views)
+# 1 - hello/ будет возврощать ответ HttResponse с текстом "Hello! Its my project"
+# 2 - now_date/ будет возврощать ответ HttpResponse c нынешней датой
+# 3 - goodby/ будет возврощать ответ HttpResponse с текстом “Goodby user!”
+#
+# отправить как GitHub репозиторий, имя репозитория будет таким “{first_name}_{last_name}_{group}”
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ products/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+import datetime
+
+def bn(reguest):
+    return HttpResponse('Hello! Its my project')
+
+def bm(reguest):
+    return HttpResponse(datetime)
+
+def bi(reguest):
+    return HttpResponse('Goodby user!')
+
+
+
